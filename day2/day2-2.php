445baca331c2ba0f653ad67e78de39339fab2034
Bokkum <82590317+Bokkum@users.noreply.github.com>
2024-12-04 10:03:05 +0100

    Update day2-2.php
    fix

--- day2/day2-2.php
+++ day2/day2-2.php
@@ -1,3 +1,4 @@
+<pre>
 <?php
 
 $input = explode("\n", file_get_contents('input.txt'));
@@ -11,19 +12,16 @@ foreach ($input as $line) {
 
 $safe = [];
 
-$unsafe = [];
-
 foreach ($reports as $report) {
     $base_array = $report;
-
-    echo "---";
+    
     $stop = false;
     for ($x = 0; $x <= (count($report) + 1); $x++) {
+
         if ($stop) {
             break;
         }
         $report = $base_array;
-        $last_level = $report[0];
         $counter = 0;
 
         $decreasing = false;
@@ -34,10 +32,7 @@ foreach ($reports as $report) {
         if ($x != 0) {
             array_splice($report, ($x - 1), 1);
         }
-
-        echo "<pre>";
-        echo print_r($report, true);
-        echo "</pre>";
+        $last_level = $report[0];
 
         foreach ($report as $level) {
             if ($counter != 0) {
@@ -51,9 +46,10 @@ foreach ($reports as $report) {
                     $increasing = true;
                 } else if ($level < $last_level) {
                     $decreasing = true;
-                } else if ($level = $last_level) {
+                } else {
                     $similar = true;
                 }
+                
                 $last_level = $level;
             }
             $counter++;
@@ -69,23 +65,9 @@ foreach ($reports as $report) {
         if (!$danger) {
             $safe[] = $report;
             $stop = true;
-            echo "<br>succes<br>";
-            $unsafe[] = $base_array;
         }
     }
-    echo "----";
 }
+$count = count($safe);
 
-echo "<pre>";
-echo print_r($unsafe, true);
-echo "</pre>";
-
-
-
-// echo "<pre>";
-// echo print_r($safe, true);
-// echo "</pre>";
-
-// $count = count($safe);
-
-// echo $count;
+echo $count;
