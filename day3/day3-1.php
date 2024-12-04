7f0d8434f3ad5714fba2bbb039b4524f5f400fcc
Bokkum <82590317+Bokkum@users.noreply.github.com>
2024-12-04 22:11:57 +0100

    day3

---
+++ day3/day3-1.php
@@ -0,0 +1,19 @@
+<?php
+$input = file_get_contents('input.txt');
+
+preg_match_all('/mul'. preg_quote('(') . '[0-9]{1,3},[0-9]{1,3}' . preg_quote(')') . '/', $input, $matches);
+
+echo "<pre>";
+echo print_r($matches[0], true);
+echo "</pre>";
+
+$multiplied = 0;
+
+foreach ($matches[0] as $match) {
+    preg_match_all('/[0-9]{1,3},[0-9]{1,3}/', $match, $line);
+    $imploded = implode($line[0]);
+    $parts = explode(',', $imploded);
+    $multiplied += $parts[0] * $parts[1];
+}
+
+echo $multiplied;
